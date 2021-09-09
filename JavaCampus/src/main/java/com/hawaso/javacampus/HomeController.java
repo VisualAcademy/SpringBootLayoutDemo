85596d5933a31945d05c5d20f564f500da08e060
RedPlus <redplus@redplus.net>
2021-09-09 22:59:00 +0900

    JavaCampus

---
+++ JavaCampus/src/main/java/com/hawaso/javacampus/HomeController.java
@@ -0,0 +1,13 @@
+package com.hawaso.javacampus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
